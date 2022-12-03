39c0302de8f630ca6f4e4990c214a62402868216
novicatepic <acivon@outlook.com>
2022-12-03 16:51:49 +0100

    File problems

--- proj_asembler/proja.asm
+++ proj_asembler/proja.asm
@@ -1,11 +1,13 @@
 SECTION .data 
     error_string db "Something went wrong!", 0
     error_string_length EQU $ - error_string
+    temp_value_for_nelems db 14
 
 SECTION .bss
     entry_file_path resq 1
     output_file_path resq 1
-    num_of_elements resb 1
+    num_of_elements resq 1
+    tmp resq 1
     x_values resq 1
     y_values resq 1
 
@@ -13,16 +15,7 @@ SECTION .text
 global _start
 _start:
 
-    ;start test
-    ;mov rax, 4
-
-    ;clean registers
-    xor rax, rax 
-    xor rbx, rbx 
-    xor rcx, rcx 
-    xor rdx, rdx 
-    xor rsi, rsi 
-    xor rdi, rdi 
+    call .clean_registers
 
     ;allocate 1 byte so we know num of elements we're working with
     mov rdi, 0
@@ -34,6 +27,16 @@ _start:
     mov rax, 9
     syscall
     mov [num_of_elements], rax 
+
+    mov rdi, 0
+    mov rsi, 1         ;allocating 1 byte
+    mov rdx, 2         ;prot value, write+read
+    mov r10, 22h
+    mov r8, -1
+    mov r9, 0
+    mov rax, 9
+    syscall
+    mov [tmp], rax 
 
     ;number of arguments
     pop rax
@@ -64,21 +67,47 @@ _start:
     mov rdi, rax
     mov rax, 0                          ;read from file
     mov rsi, [num_of_elements]
-    mov rdx, 100
+    mov rdx, 500
     syscall
 
-
-    ;closing entry file
+    ;closing entry file, need to allocate memory for other stuff
     mov rax, 3
     syscall
 
-    xor rax, rax
+
+    
+    call .clean_registers
+    ;we're going to allocate memory for all the x parameters and all the y parameters, cuz it's required 
+    ;mov rdi, 0
+    ;mov rsi, 5                          ;allocating number_of_elements bytes, 5 for fun to test
+    ;mov rdx, 2                          ;prot value, write+read
+    ;mov r10, 22h
+    ;mov r8, -1
+    ;mov r9, 0
+    ;mov rax, 9
+    ;syscall
+    ;mov [x_values], rax 
+
+    call .clean_registers
+
+    ;same code all over again, probably should make a function and put something on stack to pop it back later so I can work around that problem
+    ;mov rdi, 0
+    ;mov rsi, 5                        ;allocating number_of_elements bytes
+    ;mov rdx, 2                        ;prot value, write+read
+    ;mov r10, 22h
+    ;mov r8, -1
+    ;mov r9, 0
+    ;mov rax, 9
+    ;syscall
+    ;mov [y_values], rax 
+
+
     pop rax 
     mov [output_file_path], rax
     xor rax, rax 
     mov rax, 2
     mov rdi, [output_file_path]
-    mov rsi, 2                      ;read/write flag, only write flag also a viable option
+    mov rsi, 2                     ;read/write flag, only write flag also a viable option
     syscall                         ;open output file
 
     cmp rax, -1
@@ -87,8 +116,8 @@ _start:
 
     mov rdi, rax 
     mov rax, 1
-    mov rsi, [num_of_elements]
     mov rdx, 1
+    mov rsi, [num_of_elements] 
     syscall
 
     mov rax, 3
@@ -105,3 +134,17 @@ _start:
     mov rsi, error_string
     mov rdx, error_string_length
     syscall 
+
+.convert_str_to_int_x:
+    
+
+
+.clean_registers:
+    ;clean registers
+    xor rax, rax 
+    xor rbx, rbx 
+    xor rcx, rcx 
+    xor rdx, rdx 
+    xor rsi, rsi 
+    xor rdi, rdi 
+    ret 
